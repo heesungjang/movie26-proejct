2f13bcd91542a8b5921ae51704986a7d0d4e4894
Heesung jang <heesungj7@gmail.com>
2021-06-10 00:12:24 +0900

    current new

--- static/js/main.js
+++ static/js/main.js
@@ -21,7 +21,7 @@ function log_in() {
         },
         success: function (response) {
             if (response["result"] == "success") {
-                $.cookie("mytoken", response["token"], {path: "/"});
+                $.cookie("mytoken", response["token"], { path: "/" });
                 window.location.replace("/");
             } else {
                 alert(response["msg"]);
@@ -93,23 +93,50 @@ function get_movies() {
                     const booking = movie["booking"];
 
                     const temp_html = `
-                            <section class="card">
+                            <div class="card" style="width: 18rem">
                         <img
                             class="card-img-top"
                             src="${poster}"
                             alt="Card image cap"
                         />
-                        <h5 class="card-title">${title}</h5>
-
-                        <div class="card-body">
-                        <div class="card-info">
-                            <span class="card-genre">장르: ${genre}</span>
-                            <span class="card-producer">감독: ${producer}</span>
-                            <span class="card-booking">예매율: ${booking}%</span></div>
-                            
-                            <a href="/movie/${movie_id}" class="btn btn-primary">상세보기</a>
+                        <div style="margin-left: 10px" class="card-body">
+                            <span style="font-size: 20px" class="card-title"
+                                >${title}</span
+                            >
+                            <span
+                                style="display: block; font-size: 10px"
+                                class="card-title"
+                                >장르: ${genre}</span
+                            >
+                            <span
+                                style="
+                                    font-size: 13px;
+                                    display: block;
+                                    margin: 5px 0 0 0;
+                                "
+                                class="card-text"
+                                >감독: ${producer}</span
+                            >
+                            <span
+                                style="font-size: 12px; display: block"
+                                class="card-title"
+                                >예매율: ${booking}%</span
+                            >
+                            <a
+                                style="
+                                    display: block;
+                                    text-align: end;
+                                    margin-right: 20px;
+                                    color: inherit;
+                                    font-weight: 700;
+                                    height: 35px;
+                                "
+                                href="/movie/${movie_id}"
+                                class="btn btn-primary"
+                                >상세보기</a
+                            >
                         </div>
-                    </section>
+                    </div>
                     `;
                     if (title != "") {
                         $("#movies-box").append(temp_html);
@@ -151,11 +178,36 @@ function create_comment() {
     });
 }
 
-function re_request(movie_id) {
+function delete_comment(e) {
+    let comment_give = "";
+    if (e.target.tagName == "BUTTON") {
+        const parent = e.target.parentNode;
+        comment_give =
+            parent.firstChild.nextSibling.firstChild.nextSibling.textContent;
+    } else if (e.target.tagName == "svg") {
+        comment_give =
+            e.target.parentNode.parentNode.firstChild.nextSibling.firstChild
+                .nextSibling.textContent;
+    }
+
+    const re = /[0-9]/;
+    const url = window.location.pathname;
+    const movie_id = re.exec(url)[0];
+
     $.ajax({
-        type: "GET",
-        url: `/movie/${movie_id}`,
-        data: {},
-        success: function (response) {},
+        type: "POST",
+        url: `/movie/${movie_id}/comment/delete`,
+        data: {
+            movie_give: movie_id,
+            comment_give: comment_give,
+        },
+        success: function (response) {
+            if (response["result"] == "success") {
+                alert(response["msg"]);
+                location.reload();
+            } else if (response["result"] == "fail") {
+                alert(response["msg"]);
+            }
+        },
     });
 }
